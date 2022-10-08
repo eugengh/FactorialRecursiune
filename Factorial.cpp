0ebcde3e2a39ad6e134d50548b95bd22c7c8cdce
noname for the moment <70998165+eugengh@users.noreply.github.com>
2022-10-08 11:25:01 +0300

    Add files via upload

---
+++ Factorial.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n)
+
+{
+
+    if (n == 0)
+
+        return 1;
+
+    else
+
+        return n*factorial(n-1);
+
+}
+
+int main()
+ {
+
+    int f;
+
+    f = factorial(3);
+
+    cout<<f;
+
+    return 0;
+
+}
